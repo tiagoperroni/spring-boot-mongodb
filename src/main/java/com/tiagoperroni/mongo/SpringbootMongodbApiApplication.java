4dafd4dd54d6505518bc006d640f20d7db5caf86
tiagoperroni <tiagoperroni86@gmail.com>
2021-07-07 17:28:35 -0300

    project createad

---
+++ src/main/java/com/tiagoperroni/mongo/SpringbootMongodbApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoperroni.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbApiApplication.class, args);
+	}
+
+}
