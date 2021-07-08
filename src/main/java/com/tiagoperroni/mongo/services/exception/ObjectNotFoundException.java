97c3e8fe6af35c3cc39941112dcd9d7ccfa929dd
tiagoperroni <tiagoperroni86@gmail.com>
2021-07-08 07:43:27 -0300

    implements get user by id

---
+++ src/main/java/com/tiagoperroni/mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.tiagoperroni.mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String message) {
+		super(message);
+	}
+	
+}
