5c03f78fdb9035c00968f227338c9f17d756299a
tiagoperroni <tiagoperroni86@gmail.com>
2021-07-08 10:50:33 -0300

    getting post by id

---
+++ src/main/java/com/tiagoperroni/mongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.tiagoperroni.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagoperroni.mongo.domain.Post;
+import com.tiagoperroni.mongo.repository.PostRepository;
+import com.tiagoperroni.mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+
+}
